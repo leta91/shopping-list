02bbd9aeee26d08bcda5baec552c0cf721f9a0f9
Letizia DI MAIO <109817389+leta91@users.noreply.github.com>
2022-08-09 19:06:27 +0100

    Added JavaScript file

---
+++ shopping-list.js
@@ -0,0 +1,12 @@
+const myList = [
+    ["Water bottles", 7],
+    ["Lettuce", 10],
+    ["Pears", 10],
+    ["Oranges", 15],
+    ["Butternut squash", 3],
+    ["Goats Cheese", 3]
+  ];
+
+  const groceryShopping = myList[4][1];
+  console.log(groceryShopping);
+
